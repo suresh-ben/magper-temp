36ca7b04ad50b83a5beadc3e19b9870f8941f754
Suresh <98641509+suresh-ben@users.noreply.github.com>
2025-07-07 17:38:19 +0530

    Update expressSetup.ts

--- src/expressSetup.ts
+++ src/expressSetup.ts
@@ -34,7 +34,7 @@ function setupExpressApp(app: Express) {
         res.send("This magpet server, running successfully! :)");
     });
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log("Server is running on http://localhost:3000");
     });
 }
